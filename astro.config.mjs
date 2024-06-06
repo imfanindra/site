d141d8e3b881edb40a2d731c81b5acc253a168e6
Fanindra Maharana <78368113+imfanindra@users.noreply.github.com>
2024-06-06 11:26:57 +0530

    fix ig.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,8 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
-
 
 export default defineConfig({
   site: "https://www.fanindra.xyz/",
   integrations: [mdx(), sitemap(), tailwind()],
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
 });
