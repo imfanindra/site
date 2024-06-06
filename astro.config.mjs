3750ced507bd4e4c928e18bf06d120819460c664
Fanindra Maharana <78368113+imfanindra@users.noreply.github.com>
2024-06-06 18:22:33 +0530

    fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,14 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
-  site: "https://www.fanindra.xyz/",
-  integrations: [mdx(), sitemap(), tailwind()],
-  output: 'server',
   adapter: vercel({
     webAnalytics: {
       enabled: true,
     },
   }),
+  site: "https://www.fanindra.xyz/",
+  integrations: [mdx(), sitemap(), tailwind()],
 });
