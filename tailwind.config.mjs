a613e17ce2e8d10265390b790089f6c59ace0e80
Fanindra Maharana <78368113+imfanindra@users.noreply.github.com>
2024-09-09 16:47:01 +0530

    font change

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,7 +9,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        sans: ["aktiv-grotesk", ...defaultTheme.fontFamily.sans],
         serif: [...defaultTheme.fontFamily.serif]
       },
     },
