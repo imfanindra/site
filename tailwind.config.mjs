f1df6e7920631687ad9c022ad3a03d2dcef1e5c1
Fanindra Maharana <78368113+imfanindra@users.noreply.github.com>
2024-11-21 15:24:37 +0530

    final change before a major design change - coming soon

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,7 +9,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Geist", "CalSans", ...defaultTheme.fontFamily.sans],
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
         serif: [...defaultTheme.fontFamily.serif]
       },
     },
